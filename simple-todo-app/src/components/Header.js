ef06542614b5745a4b35a73b854fb4681f47b77c
vidushg <goswamiv@grinnell.edu>
2020-11-20 13:37:52 -0600

    added strikethroughs

--- simple-todo-app/src/components/Header.js
+++ simple-todo-app/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react"
 
 const Header = () => {
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "2em",
+  }
   return(
-    <header>
-    <h1>Simple Todo Todo </h1>
-    <p>Pink Panther List </p>
+    <header style={headerStyle}>
+    <h1 style={{fontSize: "25px", marginBottom: "15px"}}>Simple Todo Todo </h1>
+    <p style={{fontSize: "19px"}}>Pink Panther List </p>
     </header>
   )
 }
--- simple-todo-app/src/components/TodoItem.js
+++ simple-todo-app/src/components/TodoItem.js
@@ -1,14 +1,24 @@
 import React from "react"
 
 class TodoItem extends React.Component{
+
   render(){
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#d35e0f",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
     return(<li className="todo-item">
       <input type="checkbox"
       checked={this.props.todo.completed}
       onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
       <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
       Delete</button>
-      {this.props.todo.title}</li>);
+      <span style={this.props.todo.completed ? completedStyle: null}>
+      {this.props.todo.title}
+      </span>
+      </li>);
   }
 }
 
